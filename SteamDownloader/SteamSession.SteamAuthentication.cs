d9d19e0f70e60ccf2e12d964ac9fd80f5a90259b
小仙女 <me@ilyfairy.com>
2023-12-31 13:57:08 +0800

    更新

---
+++ SteamDownloader/SteamSession.SteamAuthentication.cs
@@ -0,0 +1,211 @@
+﻿using SteamKit2;
+using SteamKit2.Authentication;
+
+namespace SteamDownloader;
+
+public partial class SteamSession
+{
+    /// <summary>
+    /// 管理用户登录, 无需重新登录
+    /// </summary>
+    public class SteamAuthentication
+    {
+        private readonly SteamSession steam;
+        private bool logged = false;
+        public bool Logged => steam.steamUser.SteamID is not null && logged;
+        public string? AccessToken { get; private set; }
+
+        private bool isAnonymous;
+        private string? username;
+
+        public SteamAuthentication(SteamSession steamSession)
+        {
+            this.steam = steamSession;
+
+            //steamSession.CallbackManager.Subscribe<SteamClient.ConnectedCallback>(v =>
+            //{
+            //    Logged = true;
+            //});
+            steamSession.CallbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
+            {
+                logged = true;
+                steamSession.connectionLoginResult = v.Result;
+            });
+            steamSession.CallbackManager.Subscribe<SteamUser.LoggedOffCallback>(v =>
+            {
+                steamSession.connectionLoginResult = v.Result;
+                logged = false;
+            });
+
+        }
+
+        /// <summary>
+        /// 匿名登录
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public async Task LoginAnonymousAsync(CancellationToken cancellationToken = default)
+        {
+            if (steam.SteamClient.IsConnected is false)
+            {
+                await steam.ConnectAsync(cancellationToken);
+            }
+
+            try
+            {
+                await steam.loginLock.WaitAsync(cancellationToken);
+
+                isAnonymous = true;
+                username = null;
+                AccessToken = null;
+                steam.steamUser.LogOnAnonymous();
+
+                await Task.Run(steam.CallbackManager.RunWaitCallbacks, cancellationToken);
+
+                if (steam.connectionLoginResult is EResult.OK)
+                {
+                }
+                else if (steam.connectionLoginResult is EResult.NoConnection)
+                {
+                    throw new ConnectionException("没有连接, 请先连接");
+                }
+                else
+                {
+                    throw new ConnectionException($"登录失败: {steam.connectionLoginResult}");
+                }
+            }
+            finally
+            {
+                steam.loginLock.Release();
+            }
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="shouldRememberPassword">是否记住密码(在此之后可以用AccessToken登录)</param>
+        /// <returns></returns>
+        public async Task LoginAsync(string username, string password, bool shouldRememberPassword, CancellationToken cancellationToken = default)
+        {
+            if (!steam.SteamClient.IsConnected)
+            {
+                await steam.ConnectAsync(cancellationToken);
+            }
+
+            try
+            {
+                await steam.loginLock.WaitAsync(cancellationToken);
+
+                var authSession = await steam.SteamClient.Authentication.BeginAuthSessionViaCredentialsAsync(new AuthSessionDetails()
+                {
+                    Username = username,
+                    Password = password,
+                    IsPersistentSession = shouldRememberPassword,
+                    Authenticator = new UserConsoleAuthenticator(),
+                });
+
+                using var _ = steam.CallbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
+                {
+                    steam.connectionLoginResult = v.Result;
+                });
+
+                var result = await authSession.PollingWaitForResultAsync(cancellationToken);
+
+                AccessToken = result.RefreshToken;
+                steam.steamUser.LogOn(new SteamUser.LogOnDetails()
+                {
+                    Username = result.AccountName,
+                    Password = null,
+                    AccessToken = result.RefreshToken,
+                    ShouldRememberPassword = shouldRememberPassword,
+                });
+
+                while (true)
+                {
+                    steam.CallbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan);
+                    if (steam.connectionLoginResult is EResult.OK)
+                    {
+                        isAnonymous = true;
+                        this.username = username;
+                        break;
+                    }    
+                    if (steam.connectionLoginResult is EResult.NoConnection)
+                        throw new ConnectionException("登录失败");
+                    await Task.Delay(100, cancellationToken);
+                }
+            }
+            finally
+            {
+                steam.loginLock.Release();
+            }
+        }
+
+        public async Task LoginFromAccessTokenAsync(string username, string accessToken, CancellationToken cancellationToken = default)
+        {
+            if (!steam.SteamClient.IsConnected)
+            {
+                await steam.ConnectAsync(cancellationToken);
+            }
+
+            try
+            {
+                await steam.loginLock.WaitAsync(cancellationToken);
+
+                using var loggedOnCallbackDisposable = steam.CallbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
+                {
+                    steam.connectionLoginResult = v.Result;
+                });
+
+                steam.connectionLoginResult = EResult.Invalid;
+
+                steam.steamUser.LogOn(new SteamUser.LogOnDetails()
+                {
+                    Username = username,
+                    Password = null,
+                    AccessToken = accessToken,
+                    ShouldRememberPassword = true,
+                });
+
+                while (true)
+                {
+                    steam.CallbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan);
+                    if (steam.connectionLoginResult is EResult.OK)
+                    {
+                        this.username = username;
+                        AccessToken = accessToken;
+                        break;
+                    }
+                    if (steam.connectionLoginResult is EResult.NoConnection)
+                        throw new ConnectionException("登录失败");
+                    await Task.Delay(50, cancellationToken);
+                }
+
+            }
+            finally
+            {
+                steam.loginLock.Release();
+            }
+        }
+
+        //public async Task EnsureLoginAsync(CancellationToken cancellationToken = default)
+        //{
+        //    if (Logged)
+        //        return;
+
+        //    if (isAnonymous)
+        //    {
+        //        await LoginAnonymousAsync(cancellationToken);
+        //    }
+        //    else
+        //    {
+        //        if (username is null || AccessToken is null)
+        //            throw new ConnectionException("请先登录");
+
+        //        await LoginFromAccessTokenAsync(username, AccessToken, cancellationToken);
+        //    }
+        //}
+    }
+
+}
--- SteamDownloader/SteamSession.cs
+++ SteamDownloader/SteamSession.cs
@@ -1,5 +1,4 @@
 ﻿using SteamKit2;
-using SteamKit2.Authentication;
 using SteamKit2.CDN;
 using SteamKit2.Internal;
 using System.IO.Compression;
@@ -8,11 +7,11 @@ using System.Text.Json.Nodes;
 
 namespace SteamDownloader;
 
-public class SteamSession : IDisposable
+public partial class SteamSession : IDisposable
 {
     public HttpClient HttpClient { get; set; }
-    private readonly SteamClient steamClient;
-    private readonly CallbackManager callbackManager;
+    public SteamClient SteamClient { get; }
+    public CallbackManager CallbackManager { get; }
 
     private readonly SteamUser steamUser;
     private readonly SteamApps steamApps;
@@ -26,8 +25,8 @@ public class SteamSession : IDisposable
 
     public SteamAuthentication Authentication { get; }
 
-    public event EventHandler? Disconnected;
-    private EResult currentEResult;
+    public event EventHandler<SteamClient.DisconnectedCallback>? Disconnected;
+    private EResult connectionLoginResult;
 
     private readonly SemaphoreSlim loginLock = new(1);
 
@@ -36,48 +35,59 @@ public class SteamSession : IDisposable
     public SteamSession(SteamConfiguration? steamConfiguration = null)
     {
         HttpClient = new();
-        if(steamConfiguration is null)
+        if (steamConfiguration is null)
         {
-            steamClient = new();
+            SteamClient = new();
         }
         else
         {
-            steamClient = new(steamConfiguration);
+            SteamClient = new(steamConfiguration);
         }
-        callbackManager = new(steamClient);
+        CallbackManager = new(SteamClient);
 
-        steamUser = steamClient.GetHandler<SteamUser>() ?? throw new Exception("SteamUser获取失败");
-        steamApps = steamClient.GetHandler<SteamApps>() ?? throw new Exception("SteamApps获取失败");
-        steamContent = steamClient.GetHandler<SteamContent>() ?? throw new Exception("SteamContent获取失败");
-        steamCloud = steamClient.GetHandler<SteamCloud>() ?? throw new Exception("SteamCloud获取失败");
+        steamUser = SteamClient.GetHandler<SteamUser>() ?? throw new Exception("SteamUser获取失败");
+        steamApps = SteamClient.GetHandler<SteamApps>() ?? throw new Exception("SteamApps获取失败");
+        steamContent = SteamClient.GetHandler<SteamContent>() ?? throw new Exception("SteamContent获取失败");
+        steamCloud = SteamClient.GetHandler<SteamCloud>() ?? throw new Exception("SteamCloud获取失败");
 
-        var steamUnifiedMessages = steamClient.GetHandler<SteamUnifiedMessages>()!;
+        var steamUnifiedMessages = SteamClient.GetHandler<SteamUnifiedMessages>()!;
         publishedFile = steamUnifiedMessages.CreateService<IPublishedFile>();
 
         Authentication = new SteamAuthentication(this);
+
+        CallbackManager.Subscribe<SteamClient.DisconnectedCallback>(v =>
+        {
+            Disconnected?.Invoke(this, v);
+        });
     }
 
     public void Disconnect()
     {
-        steamClient.Disconnect();
-        callbackManager.RunWaitCallbacks(Timeout.InfiniteTimeSpan);
+        SteamClient.Disconnect();
+        EnsureRunAllCallback();
+    }
+
+    public void EnsureRunAllCallback()
+    {
+        if (SteamClient.GetCallback() is not null)
+            CallbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan);
     }
 
     public async Task ConnectAsync(CancellationToken cancellationToken = default)
     {
-        if (steamClient.IsConnected)
+        if (SteamClient.IsConnected)
             return;
 
         try
         {
             await loginLock.WaitAsync(cancellationToken);
-            steamClient.Connect();
+            SteamClient.Connect();
 
-            await Task.Run(() => callbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan), cancellationToken);
+            await Task.Run(() => CallbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan), cancellationToken);
 
-            if (steamClient.IsConnected is false)
+            if (SteamClient.IsConnected is false)
             {
-                throw new Exception("连接失败");
+                throw new ConnectionException("连接失败");
             }
         }
         catch (Exception)
@@ -90,11 +100,22 @@ public class SteamSession : IDisposable
         }
     }
 
+
+    public void CheckConnectionLogin()
+    {
+        if (SteamClient.IsConnected is false)
+            throw new ConnectionException("没有连接");
+
+        if (Authentication.Logged is false)
+            throw new ConnectionException("没有登录");
+    }
+
+
     public async Task<List<SteamContentServer>> GetCdnServersAsync(uint? cellId = null, uint? max_servers = null, CancellationToken cancellationToken = default)
     {
-        cellId ??= steamClient.CellID;
+        cellId ??= SteamClient.CellID;
 
-        var url = new Uri(steamClient.Configuration.WebAPIBaseAddress, $"/IContentServerDirectoryService/GetServersForSteamPipe/v1/?cell_id={cellId}{(max_servers is null ? "" : $"&max_servers={max_servers}")}");
+        var url = new Uri(SteamClient.Configuration.WebAPIBaseAddress, $"/IContentServerDirectoryService/GetServersForSteamPipe/v1/?cell_id={cellId}{(max_servers is null ? "" : $"&max_servers={max_servers}")}");
 
         var jsonString = await HttpClient.GetStringAsync(url, cancellationToken);
         var servers = JsonSerializer.Deserialize<List<SteamContentServer>>(JsonNode.Parse(jsonString)?["response"]?["servers"]) ?? throw new Exception("获取失败");
@@ -114,6 +135,8 @@ public class SteamSession : IDisposable
 
     public async Task<ulong> GetAppAccessTokenAsync(uint appId)
     {
+        CheckConnectionLogin();
+
         ulong appToken;
         if (!AppTokensCache.TryGetValue(appId, out appToken))
         {
@@ -139,6 +162,8 @@ public class SteamSession : IDisposable
 
     public async Task<SteamApps.PICSProductInfoCallback.PICSProductInfo> GetAppInfoAsync(uint appId)
     {
+        CheckConnectionLogin();
+
         var appToken = await GetAppAccessTokenAsync(appId);
 
         // 获取ProductInfo
@@ -147,6 +172,7 @@ public class SteamSession : IDisposable
             return productInfo;
         }
 
+        CheckConnectionLogin();
         var productInfoRequest = new SteamApps.PICSRequest(appId, appToken);
         var productInfoResult = await steamApps.PICSGetProductInfo(productInfoRequest, null);
 
@@ -198,18 +224,24 @@ public class SteamSession : IDisposable
 
     public DepotsContent GetAppInfoDepotsSection(SteamApps.PICSProductInfoCallback.PICSProductInfo appInfo)
     {
+        CheckConnectionLogin();
+
         var depots = GetAppInfoSection(appInfo, EAppInfoSection.Depots)!;
         return new DepotsContent(appInfo.ID, depots);
     }
 
     public async Task<ulong> GetManifestRequestCodeAsync(uint appId, uint depotId, ulong manifestId, string branch = "public", string? branchPasswordHash = null)
     {
+        CheckConnectionLogin();
+
         var result = await steamContent.GetManifestRequestCode(depotId, appId, manifestId, branch, branchPasswordHash);
 
         return result;
     }
     public async Task<byte[]> GetDepotKeyAsync(uint depotId, uint appId = 0)
     {
+        CheckConnectionLogin();
+
         if (DepotKeysCache.TryGetValue(depotId, out var depotKey))
         {
             return depotKey;
@@ -221,7 +253,7 @@ public class SteamSession : IDisposable
         {
             throw new Exception($"AccessDenied  DepotId:{depotId}");
         }
-        if(result.Result is not EResult.OK)
+        if (result.Result is not EResult.OK)
         {
             throw new Exception($"获取失败  DepotId:{depotId}");
         }
@@ -232,12 +264,14 @@ public class SteamSession : IDisposable
 
     public async Task<DepotManifest> GetDepotManifestEncryptAsync(uint depotId, ulong manifestId, ulong manifestRequestCode)
     {
+        CheckConnectionLogin();
+
         var server = await GetRandomCdnServer();
         const uint MANIFEST_VERSION = 5;
 
         Uri url = new(server.Url, $"/depot/{depotId}/manifest/{manifestId}/{MANIFEST_VERSION}/{manifestRequestCode}");
         var stream = await HttpClient.GetStreamAsync(url);
-        
+
         using var zip = new ZipArchive(stream, ZipArchiveMode.Read);
         var file = zip.Entries.First();
         var bytes = new byte[file.Length];
@@ -248,6 +282,8 @@ public class SteamSession : IDisposable
 
     public async Task<DepotManifest> GetDepotManifestAsync(uint depotId, ulong manifestId, ulong manifestRequestCode, byte[] depotKey)
     {
+        CheckConnectionLogin();
+
         var manifestInfo = await GetDepotManifestEncryptAsync(depotId, manifestId, manifestRequestCode);
         manifestInfo.DecryptFilenames(depotKey);
         return manifestInfo;
@@ -255,6 +291,8 @@ public class SteamSession : IDisposable
 
     public async Task<DepotManifest> GetDepotManifestAsync(uint appId, uint depotId, ulong manifestId, string branch = "public")
     {
+        CheckConnectionLogin();
+
         var code = await GetManifestRequestCodeAsync(appId, depotId, manifestId, branch);
         var manifestInfo = await GetDepotManifestEncryptAsync(depotId, manifestId, code);
         var key = await GetDepotKeyAsync(depotId, appId);
@@ -264,11 +302,15 @@ public class SteamSession : IDisposable
 
     public Task<DepotManifest> GetWorkshopManifestAsync(uint appId, ulong hcontentFileId)
     {
+        CheckConnectionLogin();
+
         return GetDepotManifestAsync(appId, appId, hcontentFileId);
     }
 
     public async Task<byte[]> DownloadChunkDecryptBytesAsync(uint depotId, DepotManifest.ChunkData chunkData, byte[] depotKey, CancellationToken cancellationToken = default)
     {
+        CheckConnectionLogin();
+
         var server = await GetRandomCdnServer(cancellationToken);
         Uri url = new(server.Url, $"/depot/{depotId}/chunk/{Convert.ToHexString(chunkData.ChunkID!)}");
 
@@ -282,6 +324,8 @@ public class SteamSession : IDisposable
 
     public Task<byte[]> DownloadChunkDecryptBytesAsync(uint depotId, DepotManifest.ChunkData chunkData, CancellationToken cancellationToken = default)
     {
+        CheckConnectionLogin();
+
         if (DepotKeysCache.TryGetValue(depotId, out var depotKey))
         {
             return DownloadChunkDecryptBytesAsync(depotId, chunkData, depotKey, cancellationToken);
@@ -301,6 +345,8 @@ public class SteamSession : IDisposable
     /// <exception cref="Exception"></exception>
     public async Task<PublishedFileDetails> GetPublishedFileAsync(uint appId, ulong pubFileId)
     {
+        CheckConnectionLogin();
+
         var request = new CPublishedFile_GetDetails_Request();
         request.appid = appId;
         request.publishedfileids.Add(pubFileId);
@@ -318,13 +364,15 @@ public class SteamSession : IDisposable
 
     public async Task<ICollection<PublishedFileDetails>> GetPublishedFileAsync(uint appId, params ulong[] pubFileIds)
     {
+        CheckConnectionLogin();
+
         var request = new CPublishedFile_GetDetails_Request();
         request.appid = appId;
         request.publishedfileids.AddRange(pubFileIds);
 
         var result = await publishedFile.SendMessage(v => v.GetDetails(request));
 
-        if(result.Result != EResult.OK)
+        if (result.Result != EResult.OK)
         {
             throw new Exception($"响应失败: {result}");
         }
@@ -337,177 +385,10 @@ public class SteamSession : IDisposable
     public void Dispose()
     {
         _disposed = true;
-        steamClient.Disconnect();
+        SteamClient.Disconnect();
         HttpClient.Dispose();
     }
 
 
 
-    public class SteamAuthentication
-    {
-        private readonly SteamSession steamSession;
-        public bool Logged { get; private set; }
-        public string? AccessToken { get; private set; }
-
-        public SteamAuthentication(SteamSession steamSession)
-        {
-            this.steamSession = steamSession;
-
-            steamSession.callbackManager.Subscribe<SteamClient.ConnectedCallback>(v =>
-            {
-                Logged = true;
-            });
-            steamSession.callbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
-            {
-                steamSession.currentEResult = v.Result;
-            });
-            steamSession.callbackManager.Subscribe<SteamUser.LoggedOffCallback>(v =>
-            {
-                steamSession.currentEResult = v.Result;
-                Logged = false;
-            });
-
-        }
-
-        /// <summary>
-        /// 匿名登录
-        /// </summary>
-        /// <returns></returns>
-        /// <exception cref="Exception"></exception>
-        public async Task LoginAnonymousAsync(CancellationToken cancellationToken = default)
-        {
-            if (steamSession.steamClient.IsConnected is false)
-            {
-                await steamSession.ConnectAsync(cancellationToken);
-            }
-
-            try
-            {
-                await steamSession.loginLock.WaitAsync(cancellationToken);
-
-                steamSession.steamUser.LogOnAnonymous();
-
-                await Task.Run(steamSession.callbackManager.RunWaitCallbacks, cancellationToken);
-
-                if (steamSession.currentEResult is EResult.OK)
-                {
-                    Logged = true;
-                }
-                else if (steamSession.currentEResult is EResult.NoConnection)
-                {
-                    throw new Exception("没有连接, 请先连接");
-                }
-                else
-                {
-                    throw new Exception($"登录失败: {steamSession.currentEResult}");
-                }
-            }
-            finally
-            {
-                steamSession.loginLock.Release();
-            }
-        }
-
-        /// <summary>
-        /// 登录
-        /// </summary>
-        /// <param name="username">用户名</param>
-        /// <param name="password">密码</param>
-        /// <param name="shouldRememberPassword">是否记住密码(在此之后可以用AccessToken登录)</param>
-        /// <returns></returns>
-        public async Task LoginAsync(string username, string password, bool shouldRememberPassword, CancellationToken cancellationToken = default)
-        {
-            if (!steamSession.steamClient.IsConnected)
-            {
-                await steamSession.ConnectAsync(cancellationToken);
-            }
-
-            try
-            {
-                await steamSession.loginLock.WaitAsync(cancellationToken);
-
-                var authSession = await steamSession.steamClient.Authentication.BeginAuthSessionViaCredentialsAsync(new AuthSessionDetails()
-                {
-                    Username = username,
-                    Password = password,
-                    IsPersistentSession = shouldRememberPassword,
-                    Authenticator = new UserConsoleAuthenticator(),
-                });
-
-                using var _ = steamSession.callbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
-                {
-                    steamSession.currentEResult = v.Result;
-                });
-
-                var result = await authSession.PollingWaitForResultAsync(cancellationToken);
-
-                AccessToken = result.RefreshToken;
-                steamSession.steamUser.LogOn(new SteamUser.LogOnDetails()
-                {
-                    Username = result.AccountName,
-                    Password = null,
-                    AccessToken = result.RefreshToken,
-                    ShouldRememberPassword = shouldRememberPassword,
-                });
-
-                while (true)
-                {
-                    steamSession.callbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan);
-                    if (steamSession.currentEResult is EResult.OK)
-                        break;
-                    if (steamSession.currentEResult is EResult.NoConnection)
-                        throw new Exception("登录失败");
-                    await Task.Delay(100, cancellationToken);
-                }
-            }
-            finally
-            {
-                steamSession.loginLock.Release();
-            }
-        }
-
-        public async Task LoginFromAccessTokenAsync(string username, string accessToken, CancellationToken cancellationToken = default)
-        {
-            if (!steamSession.steamClient.IsConnected)
-            {
-                await steamSession.ConnectAsync(cancellationToken);
-            }
-
-            try
-            {
-                await steamSession.loginLock.WaitAsync(cancellationToken);
-
-                using var loggedOnCallbackDisposable = steamSession.callbackManager.Subscribe<SteamUser.LoggedOnCallback>(v =>
-                {
-                    steamSession.currentEResult = v.Result;
-                });
-
-                steamSession.steamUser.LogOn(new SteamUser.LogOnDetails()
-                {
-                    Username = username,
-                    Password = null,
-                    AccessToken = accessToken,
-                    ShouldRememberPassword = true,
-                });
-
-                while (true)
-                {
-                    steamSession.callbackManager.RunWaitAllCallbacks(Timeout.InfiniteTimeSpan);
-                    if (steamSession.currentEResult is EResult.OK)
-                    {
-                        AccessToken = accessToken;
-                        break;
-                    }
-                    if (steamSession.currentEResult is EResult.NoConnection)
-                        throw new Exception("登录失败");
-                    await Task.Delay(50, cancellationToken);
-                }
-
-            }
-            finally
-            {
-                steamSession.loginLock.Release();
-            }
-        }
-    }
 }
